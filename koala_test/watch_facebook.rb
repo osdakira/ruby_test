0e4d574971dbbecc64ec647d5aa5aee7106082b5
Akira Osada <osd.akira@gmail.com>
2013-12-01 20:45:07 +0900

    sqlite を使わない

--- koala_test/watch_facebook.rb
+++ koala_test/watch_facebook.rb
@@ -1,45 +1,36 @@
 # -*- coding: utf-8 -*-
 require "koala"
-require "sqlite3"
 
 ACCESS_TOKEN = ENV['ACCESS_TOKEN']
-
 GROUP_ID = ENV['GROUP_ID']
 
 puts "access facebook ..."
 graph = Koala::Facebook::API.new(ACCESS_TOKEN)
-# group = graph.get_object(GROUP_ID)
 feeds = graph.get_connections(GROUP_ID, "feed")
 
 puts "reject already feeds"
-TABLE_NAME = "feeds"
-COLUMN_NAME = "feed_id"
+already_feeds_file = open("already_feeds.txt", "a+")
+already_read_ids = already_feeds_file.readlines.map(&:chomp)
 
-# TODO プレースホルダが使えない
-# db.execute("CREATE TABLE ? (?)", [TABLE_NAME, COLUMN_NAME])
-db = SQLite3::Database.new("feeds.db")
-if db.table_info(TABLE_NAME).size == 0
-  db.execute("CREATE TABLE #{TABLE_NAME} (#{COLUMN_NAME})")
-end
-
-already_read_ids = db.execute("SELECT #{COLUMN_NAME} FROM #{TABLE_NAME}").flatten
-
-new_feeds = feeds.reject{|f| f["id"] == already_read_ids}
+new_feeds = feeds.reject{|f| already_read_ids.include?(f["id"]) }
 
 puts "send mail new feeds"
-messages = new_feeds.map{|f| "#{f["from"]["name"]}\n#{f["message"][0..100]}"}
-messages = messages.join("...\n\n#{'-' * 50}\n\n")
+messages = new_feeds.map do |f|
+  <<-EOS
+  #{f["from"]["name"]}
+  #{f["message"][0..100]}...
+  #{'-' * 50}
+  EOS
+end
+messages << "https://www.facebook.com/groups/#{GROUP_ID}/"
+
+puts messages
 
 EMAILS = ["a.osada@mugenup.com"]
-emails = EMAILS.join(" ")
-
-IO.popen("mail -s 'Bug Report' #{emails}", "r+") do |io|
+IO.popen("mail -s 'Bug Report' #{EMAILS.join(" ")}", "r+") do |io|
   io.puts messages
-  io.close_write
 end
 
-puts "store feed_ids"
-new_feeds.each do |f|
-  db.execute("INSERT INTO #{TABLE_NAME} (#{COLUMN_NAME}) VALUES('#{f["id"]}')")
-end
-db.close
+puts "store feed_ids (#{new_feeds.size})"
+already_feeds_file.puts(new_feeds.map{|f| f["id"]})
+already_feeds_file.close
