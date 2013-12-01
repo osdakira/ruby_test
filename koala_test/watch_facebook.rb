411a560c190048d09a986f7494d3e337945c6f23
Akira Osada <osd.akira@gmail.com>
2013-12-01 21:56:44 +0900

    send mail by gmail

--- koala_test/watch_facebook.rb
+++ koala_test/watch_facebook.rb
@@ -1,20 +1,25 @@
 # -*- coding: utf-8 -*-
 require "koala"
+require "mail"
 
-ACCESS_TOKEN = ENV['ACCESS_TOKEN']
-GROUP_ID = ENV['GROUP_ID']
+ACCESS_TOKEN = ARGV[0]
+GROUP_ID = ARGV[1]
+USER_NAME = ARGV[2]
+PASSWORD = ARGV[3]
+EMAILS = ARGV[4..-1]
 
 puts "access facebook ..."
 graph = Koala::Facebook::API.new(ACCESS_TOKEN)
 feeds = graph.get_connections(GROUP_ID, "feed")
 
-puts "reject already feeds"
+puts "reject already feeds ..."
 already_feeds_file = open("already_feeds.txt", "a+")
 already_read_ids = already_feeds_file.readlines.map(&:chomp)
-
 new_feeds = feeds.reject{|f| already_read_ids.include?(f["id"]) }
 
-puts "send mail new feeds"
+exit if new_feeds.size == 0
+
+puts "create messages ..."
 messages = new_feeds.map do |f|
   <<-EOS
   #{f["from"]["name"]}
@@ -24,11 +29,23 @@ messages = new_feeds.map do |f|
 end
 messages << "https://www.facebook.com/groups/#{GROUP_ID}/"
 
-EMAILS = ["a.osada@mugenup.com"]
-IO.popen("mail -s 'Bug Report' #{EMAILS.join(" ")}", "r+") do |io|
-  io.puts messages
+puts "send mail new feeds ..."
+mail = Mail.new do
+  from    'no-reply@example.com'
+  to      EMAILS.join(",")
+  subject 'Bug Report'
+  body    messages.join("\n")
 end
+mail.charset = 'utf-8'
+mail.delivery_method :smtp, {
+  address:   'smtp.gmail.com',
+  port:      587,
+  domain:    'smtp.gmail.com',
+  user_name: USER_NAME,
+  password:  PASSWORD
+}
+mail.deliver!
 
-puts "store feed_ids (#{new_feeds.size})"
+puts "store feed_ids (#{new_feeds.size}) ..."
 already_feeds_file.puts(new_feeds.map{|f| f["id"]})
 already_feeds_file.close
