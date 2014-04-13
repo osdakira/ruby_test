894af78c986345a213c966aac5285de33875abd5
Akira Osada <osd.akira@gmail.com>
2014-04-13 14:49:16 +0900

    serach

--- rails_test/rails3.2/test01/app/models/user.rb
+++ rails_test/rails3.2/test01/app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name
   has_many :user_items
   has_many :items, through: :user_items
+
+  def self.search(attr, query)
+    where("`#{table_name}`.`#{attr}` LIKE ?", "%#{query}%")
+  end
 end
