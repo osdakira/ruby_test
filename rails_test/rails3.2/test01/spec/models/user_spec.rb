4b9617eb5655e63d265f3f36cda8decda2df731c
Akira Osada <osd.akira@gmail.com>
2014-04-13 16:07:13 +0900

    name でも 同じ

--- rails_test/rails3.2/test01/spec/models/user_spec.rb
+++ rails_test/rails3.2/test01/spec/models/user_spec.rb
@@ -18,7 +18,7 @@ describe User do
   end
 
   context "includes" do
-    it "like name" do
+    it "like includes name" do
       subject = User.includes(:user_items).where("`name` LIKE ?", "%user0%")
       expect(subject.explain).to include "SELECT `users`.* FROM `users`  WHERE (`name` LIKE '%user0%')"
       expect(subject).to eq [user]
@@ -28,6 +28,12 @@ describe User do
       subject = User.includes(:user_items).where("`email` LIKE ?", "%user0@example.com%")
       expect(subject.explain).to include "SELECT `users`.`id` AS t0_r0, `users`.`name` AS t0_r1, `users`.`email` AS t0_r2, `users`.`created_at` AS t0_r3, `users`.`updated_at` AS t0_r4, `user_items`.`id` AS t1_r0, `user_items`.`user_id` AS t1_r1, `user_items`.`item_id` AS t1_r2, `user_items`.`number` AS t1_r3, `user_items`.`created_at` AS t1_r4, `user_items`.`updated_at` AS t1_r5 FROM `users` LEFT OUTER JOIN `user_items` ON `user_items`.`user_id` = `users`.`id` WHERE (`email` LIKE '%user0@example.com%')"
       expect(subject).to eq [user]
+    end
+
+    it "like includes name" do
+      subject = User.includes(:user_items).where("`name` LIKE ?", "%user0@example.com%")
+      expect(subject.explain).to include "SELECT `users`.`id` AS t0_r0, `users`.`name` AS t0_r1, `users`.`email` AS t0_r2, `users`.`created_at` AS t0_r3, `users`.`updated_at` AS t0_r4, `user_items`.`id` AS t1_r0, `user_items`.`user_id` AS t1_r1, `user_items`.`item_id` AS t1_r2, `user_items`.`number` AS t1_r3, `user_items`.`created_at` AS t1_r4, `user_items`.`updated_at` AS t1_r5 FROM `users` LEFT OUTER JOIN `user_items` ON `user_items`.`user_id` = `users`.`id` WHERE (`name` LIKE '%user0@example.com%')"
+      expect(subject).to eq []
     end
   end
 end
