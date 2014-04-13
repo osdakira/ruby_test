294bc809175615ee9b7aaff93889439ca9cf14f6
Akira Osada <osd.akira@gmail.com>
2014-04-13 14:36:14 +0900

    make sample data

--- rails_test/rails3.2/test01/db/seeds.rb
+++ rails_test/rails3.2/test01/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+3.times.each do | i |
+  User.create(name: "user#{i}", email: "user#{i}@example.com")
+  Item.create(name: "item#{i}")
+end
+
+items = Item.all.to_a
+User.all.to_a.each do | user |
+  user.items << items.sample
+end
