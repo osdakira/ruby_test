ce68c9fa62ec4bad670cac2c1e63f5bc8bdbaf0f
Akira Osada <osd.akira@gmail.com>
2014-04-13 14:36:00 +0900

    has_many thorugh

--- rails_test/rails3.2/test01/app/models/item.rb
+++ rails_test/rails3.2/test01/app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
   attr_accessible :name
   has_many :user_items
+  has_many :users, through: :user_items
 end
--- rails_test/rails3.2/test01/app/models/user.rb
+++ rails_test/rails3.2/test01/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
   has_many :user_items
+  has_many :items, through: :user_items
 end
