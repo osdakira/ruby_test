f0108d935df3989841cf1303337f251c2611f68e
Akira Osada <osd.akira@gmail.com>
2014-08-26 21:27:09 +0900

    if と 後置 if

--- practice/if_expression/return_if.rb
+++ practice/if_expression/return_if.rb
@@ -1,18 +1,31 @@
-class A
-  def if_expression(flag)
-    if flag
-      "OK"
-    else
-      "NG"
-    end
-  end
+# class A
+#   def if_expression(flag)
+#     if flag
+#       "OK"
+#     else
+#       "NG"
+#     end
+#   end
 
-  def if_modifier(flag)
+#   def if_modifier(flag)
+#     "NG"
+#     "OK" if flag
+#   end
+
+#   def plus(a, b)
+#     a + b
+#   end
+# end
+
+def if_expression(flag)
+  if flag
     "NG"
-    "OK" if flag
+  else
+    "OK"
   end
+end
 
-  def plus(a, b)
-    a + b
-  end
+def if_modifier(flag)
+  "OK"
+  "NG" if flag
 end
