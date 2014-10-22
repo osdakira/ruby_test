49c5d8b81f78253c35dfe31838f2ab13c579e9f3
Akira Osada <osd.akira@gmail.com>
2014-10-22 22:48:37 +0900

    refactor

--- rspec_test/spec/stub_spec.rb
+++ rspec_test/spec/stub_spec.rb
@@ -1,7 +1,10 @@
 describe "Stub" do
   class A; end
   class B < A; end
-  before { allow_any_instance_of(A).to receive(:object_id) { 1 } }
-  it { expect(A.new.object_id).to eq 1 }
-  it { expect(B.new.object_id).to eq 1 }
+
+  describe "instance method" do
+    before { allow_any_instance_of(A).to receive(:object_id) { 1 } }
+    it { expect(A.new.object_id).to eq 1 }
+    it { expect(B.new.object_id).to eq 1 }
+  end
 end
