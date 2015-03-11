79d9d0747d9087e666d284c7fa523f425d6932e8
Akira Osada <osd.akira@gmail.com>
2015-03-11 22:26:24 +0900

    associations

--- rails_test/rails4.2.0/test01/app/models/item.rb
+++ rails_test/rails4.2.0/test01/app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ActiveRecord::Base
+  has_many :users_items
+  has_many :users, through: :users_items
 end
--- rails_test/rails4.2.0/test01/app/models/user.rb
+++ rails_test/rails4.2.0/test01/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :users_items
+  has_many :items, through: :users_items
 end
