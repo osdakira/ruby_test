fd6ddee7fd9e931f4cdb9a777319408d6998f921
Akira Osada <osd.akira@gmail.com>
2015-06-19 23:12:52 +0900

    delete all

--- rails_test/rails4.2.1/app/models/action_log.rb
+++
@@ -1,3 +0,0 @@
-class ActionLog < ActiveRecord::Base
-  belongs_to :user
-end
--- rails_test/rails4.2.1/db/migrate/20150612114230_create_action_logs.rb
+++
@@ -1,9 +0,0 @@
-class CreateActionLogs < ActiveRecord::Migration
-  def change
-    create_table :action_logs do |t|
-      t.references :user, index: true, foreign_key: true
-
-      t.timestamps null: false
-    end
-  end
-end
