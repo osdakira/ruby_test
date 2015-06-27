bca77e923a1bb0fd1d09044900827d2cd4299024
Akira Osada <osd.akira@gmail.com>
2015-06-28 08:39:19 +0900

    scope

--- rails_test/rails4.2.2/test01/app/models/user.rb
+++ rails_test/rails4.2.2/test01/app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+
+  scope :scope_A, -> { where(bust: 72..74) }
+  scope :scope_B, -> { where(waist: 58..60) }
+  scope :scope_C, -> { where(hip: 87..90) }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
