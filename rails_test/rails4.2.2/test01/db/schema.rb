c00abe76ccb44e9d2a643d0e0bc5988b3f90a33d
Akira Osada <osd.akira@gmail.com>
2015-08-26 20:43:20 +0900

    rake db:migrate

--- rails_test/rails4.2.2/test01/db/schema.rb
+++ rails_test/rails4.2.2/test01/db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626131242) do
+ActiveRecord::Schema.define(version: 20150826114304) do
+
+  create_table "blorgh_articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "blorgh_comments", force: :cascade do |t|
+    t.integer  "article_id"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
