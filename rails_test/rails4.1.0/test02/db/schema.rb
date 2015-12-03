1fe5df12c0547816730800f4a2368b142488d607
Akira Osada <osd.akira@gmail.com>
2015-12-03 22:55:44 +0900

    rake db:migrate

--- rails_test/rails4.1.0/test02/db/schema.rb
+++ rails_test/rails4.1.0/test02/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203135230) do
+ActiveRecord::Schema.define(version: 20151203135532) do
+
+  create_table "answers", force: true do |t|
+    t.integer  "question_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "question_translations", force: true do |t|
     t.integer  "question_id"
