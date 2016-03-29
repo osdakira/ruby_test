d2736acebb0ca6c8f1e4681995ecfcd2050b0303
Akira Osada <osd.akira@gmail.com>
2016-03-29 22:34:38 +0900

    test01

--- rails_test/rails4.2.2/test01/db/schema.rb
+++ rails_test/rails4.2.2/test01/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826114947) do
+ActiveRecord::Schema.define(version: 20160305231025) do
 
   create_table "blorgh_articles", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,25 @@ ActiveRecord::Schema.define(version: 20150826114947) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "goods", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "goods", ["post_id"], name: "index_goods_on_post_id"
+  add_index "goods", ["user_id"], name: "index_goods_on_user_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
