945d14820b5fa3897a3a2b4cc0b4be41c595cc00
Akira Osada <osd.akira@gmail.com>
2016-04-08 21:10:10 +0900

    GC

--- newapp/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Newapp::Application.config.secret_token = '7d04bdaced2cabfae726c6a72a52d09d3016dffee6e10fd65212ee5adb8d4cbd4858ec5a2d7aa258611f6b29d1ccf5d9d0ae2e625635580dd3b9c917e97cbe91'
